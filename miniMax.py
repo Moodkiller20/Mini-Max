9d2c9579a124369f89f19bdc9f83026934bc6ff1
giovanni  tshibangu <61130107+Moodkiller20@users.noreply.github.com>
2022-05-19 20:25:02 -0700

    Create miniMax.py

---
+++ miniMax.py
@@ -0,0 +1,22 @@
+# Array of 5 elements
+arr=[1,2,3,4,5]
+
+# Function
+def miniMaxSum(arr):
+    temp =[] # temporary empty array 
+ 
+    addVal = 0
+    
+    # Loop through the array and sum up all 5 elements
+    for key in range(0,len(arr)):
+       addVal += arr[key]
+        
+    # Sum everything except one elements arr[0-4] and each to the temp array
+    for i in range(0,len(arr)):
+        num= addVal- arr[i]
+        temp.append(num)
+
+    temp.sort()           # Sort array 
+    print(temp[0], temp[-1])   # print two number (min,  max)
+
+miniMaxSum(arr)
